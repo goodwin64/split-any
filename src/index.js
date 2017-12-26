a2550e98e00312549db23fee4bcebf8337ce4227
Maksym_Donchenko <maksym_donchenko@epam.com>
2017-12-26 15:30:09 +0200

    ES5 syntax downgrade

--- src/index.js
+++ src/index.js
@@ -1,44 +1,56 @@
+'use strict';
+
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
 module.exports = splitAny;
 
 function splitAny(any) {
-    switch (typeof any) {
-        case 'string': {
+    switch (typeof any === 'undefined' ? 'undefined' : _typeof(any)) {
+        case 'string':
+        {
             return splitString(any);
         }
-        case 'number': {
+        case 'number':
+        {
             return splitNumber(any);
         }
-        case 'function': {
+        case 'function':
+        {
             return splitFunction(any);
         }
-        case 'object': {
+        case 'object':
+        {
             if (Array.isArray(any)) {
                 return splitArray(any);
             } else {
                 return splitMap(any);
             }
         }
-        default: {
+        default:
+        {
             throw new TypeError('Unexpected type');
         }
     }
 }
 
-function splitString(str, delim = ' ') {
-    const subStrings = str.split(delim);
+function splitString(str) {
+    var delim = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : ' ';
+
+    var subStrings = str.split(delim);
     if (subStrings.length > 1) {
-        return subStrings.map(subString => splitString(subString));
+        return subStrings.map(function (subString) {
+            return splitString(subString);
+        });
     } else {
         return str.split('');
     }
 }
 
 function splitNumber(num) {
-    const isNegative = num < 0;
-    return [
-        ...(isNegative ? '-' : ''),
-        ...String(num).split(''),
-    ];
+    var isNegative = num < 0;
+    return [].concat(_toConsumableArray(isNegative ? '-' : ''), _toConsumableArray(String(num).split('')));
 }
 
 function splitFunction(fn) {
@@ -46,12 +58,13 @@ function splitFunction(fn) {
 }
 
 function splitMap(any) {
-    return Object.keys(any).map(key => [
-        key,
-        any[key]
-    ]);
+    return Object.keys(any).map(function (key) {
+        return [key, any[key]];
+    });
 }
 
 function splitArray(any) {
-    return any.map(arrayElement => splitAny(arrayElement));
+    return any.map(function (arrayElement) {
+        return splitAny(arrayElement);
+    });
 }
