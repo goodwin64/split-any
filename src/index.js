069a406ed0d302e11e7831eecda97374bc466017
Maksym_Donchenko <maksym_donchenko@epam.com>
2017-12-26 10:49:44 +0200

    split strings

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function splitAny(any) {
-    // your solution here
+    switch (typeof any) {
+        case 'string': {
+            return splitString(any);
+        }
+        default: {
+            throw new TypeError('Unexpected type');
+        }
+    }
+};
+
+function splitString(str, delim = ' ') {
+    const subStrings = str.split(delim);
+    if (subStrings.length > 1) {
+        return subStrings.map(subString => splitString(subString))
+    } else {
+        return str.split('');
+    }
 }
